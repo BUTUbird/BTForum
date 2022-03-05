a9db15a378be4d86228e8d0bce25f0c0493f9f3f
BUTU <1781894948@qq.com>
2022-03-06 02:00:49 +0800

    个人主页

--- bt-forum/src/main/java/org/butu/controller/UserController.java
+++ bt-forum/src/main/java/org/butu/controller/UserController.java
@@ -1,12 +1,17 @@
 package org.butu.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.butu.common.api.ApiResult;
 import org.butu.model.dto.LoginDTO;
 import org.butu.model.dto.RegisterDTO;
+import org.butu.model.entity.Post;
 import org.butu.model.entity.User;
+import org.butu.service.PostService;
 import org.butu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +34,8 @@ import static org.butu.utils.JwtUtil.USER_NAME;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private PostService postService;
 
     @PostMapping("/register")
     public ApiResult<Map<String, Object>>register(@Valid @RequestBody RegisterDTO dto){
@@ -59,5 +66,24 @@ public class UserController {
     @GetMapping("/logout")
     public ApiResult<Object>logout(){
         return ApiResult.success(null,"注销成功");
+    }
+
+    @GetMapping("/{username}")
+    public ApiResult<Map<String, Object>> getUserByName(@PathVariable("username") String username,
+                                                        @RequestParam(value = "pageNo", defaultValue = "1") Integer pageNo,
+                                                        @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        Map<String, Object> map = new HashMap<>(16);
+        User user = userService.getUserByUsername(username);
+        Assert.notNull(user, "用户不存在");
+        Page<Post> page = postService.page(new Page<>(pageNo, size),
+                new LambdaQueryWrapper<Post>().eq(Post::getUserId, user.getId()));
+        map.put("user", user);
+        map.put("topics", page);
+        return ApiResult.success(map);
+    }
+    @PostMapping("/update")
+    public ApiResult<User> updateUser(@RequestBody User umsUser) {
+        userService.updateById(umsUser);
+        return ApiResult.success(umsUser);
     }
 }
