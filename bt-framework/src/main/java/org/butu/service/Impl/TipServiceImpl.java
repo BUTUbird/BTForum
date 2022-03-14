322b2424f092c8fe64f05bf0cc13773e4de53701
BUTU <1781894948@qq.com>
2022-03-14 21:09:28 +0800

    增加使用redis保存每日一言，每24小事刷新一次

--- bt-framework/src/main/java/org/butu/service/Impl/TipServiceImpl.java
+++ bt-framework/src/main/java/org/butu/service/Impl/TipServiceImpl.java
@@ -1,11 +1,14 @@
 package org.butu.service.Impl;
 
 import lombok.extern.slf4j.Slf4j;
+import org.butu.config.redis.RedisService;
 import org.butu.model.entity.Tip;
 import org.butu.mapper.TipMapper;
 import org.butu.service.TipService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 /**
  * <p>
@@ -18,12 +21,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class TipServiceImpl extends ServiceImpl<TipMapper, Tip> implements TipService {
+    @Autowired
+    private RedisService redisService;
 
     @Override
     public Tip getRandomTip() {
         Tip todayTip = null;
         try {
-            todayTip = this.baseMapper.getRandomTip();
+            todayTip = (Tip) redisService.get("today_tip");
+            if (ObjectUtils.isEmpty(todayTip)) {
+                todayTip = this.baseMapper.getRandomTip();
+                redisService.set("today_tip", todayTip, 24 * 60 * 60);
+            }
+
         }catch (Exception e) {
             log.info("tip转化失败");
         }
